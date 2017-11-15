fe26f2378a7199bf63f31bbbe346714135ecaa79
iAmNathanJ <jacobs@heysparkbox.com>
2017-11-14 23:28:12 -0500

    add social links

--- src/js/components/site-footer.js
+++ src/js/components/site-footer.js
@@ -13,10 +13,10 @@ export default class SiteFooter extends PureComponent {
     return (
       <footer className="site-footer">
         <ul className="site-footer__icons">
-          <li className="icon"><a href=""><Sparkbox /></a></li>
-          <li className="icon"><a href=""><Codepen /></a></li>
-          <li className="icon"><a href=""><GitHub /></a></li>
-          <li className="icon"><a href=""><Twitter /></a></li>
+          <li className="icon"><a href="https://seesparkbox.com/foundry/search/results/b36ca9c45bda44fb6d09889b48e2738c/"><Sparkbox /></a></li>
+          <li className="icon"><a href="https://github.com/iAmNathanJ"><GitHub /></a></li>
+          <li className="icon"><a href="https://codepen.io/iAmNathanJ/"><Codepen /></a></li>
+          <li className="icon"><a href="https://twitter.com/nathanAlan"><Twitter /></a></li>
         </ul>
       </footer>
     );
