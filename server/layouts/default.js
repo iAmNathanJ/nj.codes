8bcee0c1bd2bcce24f0942843ab53a2c4885ca66
iAmNathanJ <jacobs@heysparkbox.com>
2017-11-15 22:58:06 -0500

    Add media specific CSS link

--- server/layouts/default.js
+++ server/layouts/default.js
@@ -5,8 +5,10 @@ export default ({ css, data = {}, body }) => `
     <meta charset="utf-8">
     <title>nj.codes</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="subresource" href="/js/main.js">
     <link rel="preload" href="/js/main.js" as="script">
     <style>${css}</style>
+    <link rel="stylesheet" type="text/css" media="(min-width:415px)" href="/css/wide.css">
   </head>
   <body>
     <div id="root">${body}</div>
--- tasks/postcss.js
+++ tasks/postcss.js
@@ -1,6 +1,17 @@
 const postcss = require('postcss');
 const autoprefixer = require('autoprefixer');
+const splitMq = require('postcss-split-mq');
 
 module.exports = async (css) => {
-  return await postcss([autoprefixer]).process(css);
+  return await postcss([
+    autoprefixer(),
+    splitMq({
+      outpath: './dist/css',
+      files: [{
+        name: 'wide.css',
+        // everything over 414
+        match: /min-width:\s*(4(1[5-9]|[2-9][0-9])|[5-9]\d{2}|1\d{3})(\.\d+)?px/
+      }]
+    })
+  ]).process(css);
 };
