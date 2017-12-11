c98a06edc40af2c6f580ed7df8b65bc362166135
iAmNathanJ <jacobs@heysparkbox.com>
2017-12-10 22:15:19 -0500

    Hard code home page intro

--- src/js/components/home.js
+++ src/js/components/home.js
@@ -14,7 +14,13 @@ export default class Home extends PureComponent {
       <Page pageName={route.pageName}>
         <div className="contain">
           <div className="intro border-top-accent">
-            <div className="markdown" dangerouslySetInnerHTML={data.intro} />
+            <div className="markdown">
+              <h2>Hello!</h2>
+              <p>I'm Nate.</p>
+              <p>I like JavaScript.</p>
+              <p>I work at <a href="https://seesparkbox.com">Sparkbox</a> where I get to write a lot of JavaScript.</p>
+              <p>Sometimes <Link to="/articles">I write about JavaScript (or other code related stuff)</Link>.</p>
+            </div>
           </div>
         </div>
       </Page>
