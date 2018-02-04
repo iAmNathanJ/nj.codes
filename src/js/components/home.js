0385b72430c4be79e90701337fd358c1358f9456
iAmNathanJ <jacobs.123@gmail.com>
2018-02-04 14:52:55 -0500

    Add long shadow effect to home page

--- src/js/components/home.js
+++ src/js/components/home.js
@@ -2,10 +2,15 @@ import React, { PureComponent } from 'react';
 import Page from 'components/page';
 import changeTheme from 'helpers/change-theme';
 import { Link } from 'react-router-dom';
+import { createShadow } from 'helpers/long-shadow';
 
 export default class Home extends PureComponent {
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount() {
+    createShadow(this.hi);
   }
 
   render() {
@@ -15,11 +20,13 @@ export default class Home extends PureComponent {
         <div className="contain">
           <div className="intro border-top-accent">
             <div className="markdown">
-              <h2>Hello!</h2>
-              <p>I'm Nate.</p>
-              <p>I like JavaScript.</p>
-              <p>I work at <a href="https://seesparkbox.com">Sparkbox</a> where I get to write a lot of JavaScript.</p>
-              <p>Sometimes <Link to="/articles">I write about JavaScript (or other code related stuff)</Link>.</p>
+              <h2 className="hi" ref={node => this.hi = node}>Hi Friends!</h2>
+              <p>
+                I'm Nate.<br />
+                I like JavaScript.<br />
+                I work at <a href="https://seesparkbox.com">Sparkbox</a>, where I get to write a lot of JavaScript.<br />
+                Sometimes <Link to="/articles">I write about JavaScript (or other code related stuff)</Link>.
+              </p>
             </div>
           </div>
         </div>
