940d7b1fa0dd9de56582dfcd74c7f55817acbf3a
iAmNathanJ <jacobs.123@gmail.com>
2018-02-04 22:05:46 -0500

    Update mocks, read drafts in dev

--- server/routes/mock.js
+++ server/routes/mock.js
@@ -1,6 +1,7 @@
-import client from '../../lib/gh-client';
-import { read } from '../../lib/io';
+import { resolve, join, basename } from 'path';
+import { dir, read } from '../../lib/io';
 import marked from 'marked';
+import fm from 'front-matter';
 
 export default function(router) {
   if (ENV === 'production') return;
@@ -11,6 +12,42 @@ export default function(router) {
       res.json({
         __html: marked(intro)
       });
+    } catch (e) {
+      console.error(e);
+      res.end('uh oh');
+    }
+  });
+
+  router.get('/api/articles', async (req, res) => {
+    try {
+      const [ drafts, articles ] = await Promise.all([
+        dir('./src/drafts'),
+        dir('./src/articles')
+      ]);
+      const entries = await Promise.all(
+        [...drafts, ...articles].map(async file => ({
+          content: await read(file),
+          filename: basename(file)
+        }))
+      );
+      res.json(entries.map((entry, i) => {
+        const oid = i;
+        const { filename } = entry;
+        const { attributes, body } = fm(entry.content);
+        const [ path ] = filename.split('.');
+        return {
+          ...attributes,
+          body,
+          filename,
+          oid,
+          path,
+          url: `https://nj.codes/articles/${path}`,
+          articleLink: `https://github.com/iAmNathanJ/nj.codes/blob/master/src/articles/${filename}`,
+          __html: marked(body),
+        };
+      }).sort((a, b) => {
+        return b['date-authored'] - a['date-authored'];
+      }));
     } catch (e) {
       console.error(e);
       res.end('uh oh');
