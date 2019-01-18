5e164944d4b53f9e2698f49bf5e02738c317cfac
iAmNathanJ <jacobs.123@gmail.com>
2019-01-18 00:50:09 -0500

    Fix: show article history if more than 0 revisions have happend

--- src/components/Revisions.js
+++ src/components/Revisions.js
@@ -16,7 +16,7 @@ const Revisions = ({ revisions, file }) => {
         {latestRevision.sha1}
       </a>
       <br/>
-      {pastRevisions.length > 1 && (
+      {pastRevisions.length > 0 && (
         <Fragment>
           <button
             aria-label={`${open ? 'close' : 'open'} history`}
